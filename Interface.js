9c5c6cd30a0886d1392b0fb351ffc4237206ef70
zhoujj <1254883879@qq.com>
2019-04-28 09:04:40 +0800

    门面模式

--- Interface.js
+++ Interface.js
@@ -54,3 +54,61 @@ function extend(subClass, superClass){
         superClass.prototype.constructor = superClass;
     }
 }
+
+
+var DED = window.DED || {};
+DED.util.Event = DED.util.Event || {};
+DED.util.Event = {
+	getEvent: function(e){
+		return e || window.event;
+	},
+	getTarget: function(e){
+		return e.target || e.srcElement;
+	},
+	/**
+	 * 中止事件沿DOM树向上冒泡的传播过程
+	 */
+	stopPropagation: function(e){
+		if(e.stopPropagation){//W3 interface
+			e.stopPropagation();
+		}else{
+			e.cancelBubble = true;//IE‘s interface
+		}
+	},
+	/**
+	 * 阻止浏览器针对一个事件的默认行为
+	 * 		可以防止对连接的点击导致浏览器导航到一个新的页面
+	 *		可以阻止表单的提交
+	 */
+	preventDefault: function(e){
+		if(e.preventDefault){//W3
+			e.preventDefault();
+		}else{
+			e.returnValue = false;//IE
+		}
+	},
+	stopEvent: function(e){
+		DED.util.stopPropagation();
+		DED.util.preventDefault();
+	}
+};
+
+//设置样式
+function setStyle(ids, prop, val){
+	if(typeof ids == 'string'){
+		ids = [ids];
+	}
+	for(var i = 0, len = ids.length; i < len; i++){
+		document.getElementById(ids[i]).style[prop] = val;
+	}
+}
+
+function setCSS(ids, styles){
+	if(typeof ids == 'string'){
+		ids = [ids];
+	}
+	for(var prop in styles){
+		if(!styles.hasOwnProperty(prop)) continue;
+		setStyle(ids, prop, styles[prop]);
+	}
+}
